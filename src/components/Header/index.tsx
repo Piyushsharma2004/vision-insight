35acdfee2f7366ae0402f06b9c667c1e37463315
piyushsharma2004 <piyushsharmanova@gmail.com>
2025-01-15 09:33:32 +0530

    Update header styling and modify subtitle for clarity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Header = (props: {
 }) => {
   return (
     <header className="sticky top-0 z-999 flex w-full border-b border-stroke bg-white dark:border-stroke-dark dark:bg-gray-dark">
-      <div className="flex flex-grow items-center justify-between px-4 py-5 shadow-2 md:px-5 2xl:px-10">
+      <div className="flex flex-grow items-center justify-between px-4 py-2 shadow-2 md:px-5 2xl:px-10">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -71,7 +71,7 @@ const Header = (props: {
             <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
               Dashboard
             </h1>
-            <p className="font-medium">Next.js Admin Dashboard Solution</p>
+            <p className="font-medium">Vision Insight Infrastructure Optimization</p>
           </div>
         </div>
 
